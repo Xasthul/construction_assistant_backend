0d9518f53dc376b2cd24a1bac433e1b43739dd55
nazhon <nazargon2003@gmail.com>
2024-04-14 14:00:44 +0300

    Inject step repository in steps service

--- src/api/steps/steps.service.ts
+++ src/api/steps/steps.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Step } from './step.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StepsService {}
+export class StepsService {
+    constructor(
+        @InjectRepository(Step)
+        private stepRepository: Repository<Step>
+    ) { }
+}
