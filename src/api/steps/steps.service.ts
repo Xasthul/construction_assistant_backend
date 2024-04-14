543abe4647427bb5a5ef02fd367ceab672031b54
nazhon <nazargon2003@gmail.com>
2024-04-14 14:05:02 +0300

    Add create in steps service

--- src/api/steps/steps.service.ts
+++ src/api/steps/steps.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Step } from './step.entity';
 import { Repository } from 'typeorm';
+import { CreateStepDto } from './dto/create-step.dto';
 
 @Injectable()
 export class StepsService {
@@ -9,4 +10,12 @@ export class StepsService {
         @InjectRepository(Step)
         private stepRepository: Repository<Step>
     ) { }
+
+    async create(createStepDto: CreateStepDto) {
+        const step = new Step();
+        step.title = createStepDto.title;
+        step.details = createStepDto.details;
+        step.assets = createStepDto.assets;
+        return await this.stepRepository.save(step);
+    }
 }
