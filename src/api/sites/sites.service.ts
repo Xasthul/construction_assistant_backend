22c81856834f3922aa762440924156ee3e61ed32
nazhon <nazargon2003@gmail.com>
2024-04-14 21:27:08 +0300

    Inject repository in sites service

--- src/api/sites/sites.service.ts
+++ src/api/sites/sites.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Site } from './site.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SitesService {}
+export class SitesService {
+    constructor(
+        @InjectRepository(Site)
+        private siteRepository: Repository<Site>
+    ) { }
+}
