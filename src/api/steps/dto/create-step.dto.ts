d2a27913527964aa20e3478e78f41a48eb966f5b
nazhon <nazargon2003@gmail.com>
2024-04-14 14:16:06 +0300

    Make details and assets optional in create step dto

--- src/api/steps/dto/create-step.dto.ts
+++ src/api/steps/dto/create-step.dto.ts
@@ -1,5 +1,17 @@
+import { IsArray, IsNotEmpty, IsOptional, IsString } from "class-validator"
+
 export class CreateStepDto {
+
+    @IsString()
+    @IsNotEmpty()
     readonly title: string
+
+    @IsString()
+    @IsOptional()
     readonly details: string
+
+    @IsArray()
+    @IsString()
+    @IsOptional()
     readonly assets: string[]
 }
