1b91a9fc39a6a818b4225af38749bec99ad9bc59
nazhon <nazargon2003@gmail.com>
2024-04-14 14:05:14 +0300

    Add create in steps controller

--- src/api/steps/steps.controller.ts
+++ src/api/steps/steps.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { StepsService } from './steps.service';
+import { CreateStepDto } from './dto/create-step.dto';
 
 @Controller('steps')
-export class StepsController {}
+export class StepsController {
+    constructor(readonly stepsService: StepsService) { }
+
+    @Post()
+    create(@Body() createStepDto: CreateStepDto) {
+        return this.stepsService.create(createStepDto);
+    }
+}
