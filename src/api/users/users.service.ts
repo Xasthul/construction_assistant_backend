89355879093f5ea0fe1bd7e3e3c4bce161cf660b
nazhon <nazargon2003@gmail.com>
2024-04-16 23:36:52 +0300

    Add create in users service

--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/domain/models/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
+
+    async create(createUserDto: CreateUserDto) {
+        const user = new User();
+        user.email = createUserDto.email;
+
+        const salt = await bcrypt.genSalt();
+        user.password = await bcrypt.hash(createUserDto.password, salt);
+        return await this.usersRepository.save(user);
+    }
+}
