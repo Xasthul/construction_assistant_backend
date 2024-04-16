0b91eacfbcc4346e0787c40e1276c7b5ef002b88
nazhon <nazargon2003@gmail.com>
2024-04-16 23:38:52 +0300

    And signup route in auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private usersService: UsersService) { }
+
+    @Post('signup')
+    signUp(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
+    }
+}
