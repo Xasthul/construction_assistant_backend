4e937d97ffa8343261bfa73797f8d22a5ad13385
nazhon <nazargon2003@gmail.com>
2024-04-16 23:40:47 +0300

    Add requirements for password validation in create user dto

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -5,13 +5,12 @@ export class CreateUserDto {
     @IsEmail()
     readonly email: string
 
-    // @IsStrongPassword({
-    //     minLength: 8,
-    //     minLowercase: 1,
-    //     minNumbers: 1,
-    //     minSymbols: 1,
-    //     minUppercase: 1
-    // })
-    @IsStrongPassword()
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        minUppercase: 1
+    })
     readonly password: string
 }
