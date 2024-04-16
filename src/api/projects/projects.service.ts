055e86315e29b32d777ccd9eae4a42b9121e9eb2
nazhon <nazargon2003@gmail.com>
2024-04-16 20:35:15 +0300

    Add create and delete for projects service

--- src/api/projects/projects.service.ts
+++ src/api/projects/projects.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectsService {
-    
+    constructor(
+        @InjectRepository(Project)
+        private projectRepository: Repository<Project>
+    ) { }
+
+    async create(createProjectDto: CreateProjectDto) {
+        const project = new Project();
+        project.title = createProjectDto.title;
+        return await this.projectRepository.save(project);
+    }
+
+    async delete(id: number) {
+        return await this.projectRepository.delete({ id: id });
+    }
 }
