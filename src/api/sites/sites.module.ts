b526be7cd011b49c401b50faff1919880da5eba8
nazhon <nazargon2003@gmail.com>
2024-04-16 20:35:58 +0300

    Register project entity in sites module

--- src/api/sites/sites.module.ts
+++ src/api/sites/sites.module.ts
@@ -3,9 +3,10 @@ import { SitesService } from './sites.service';
 import { SitesController } from './sites.controller';
 import { Site } from './site.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from '../projects/project.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Site])],
+  imports: [TypeOrmModule.forFeature([Project, Site])],
   providers: [SitesService],
   controllers: [SitesController]
 })
