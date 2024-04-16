8036a0cf3e2ebd373f1cc24a1ef2d097021869e3
nazhon <nazargon2003@gmail.com>
2024-04-16 20:36:12 +0300

    Add create in sites service

--- src/api/sites/sites.service.ts
+++ src/api/sites/sites.service.ts
@@ -2,11 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Site } from './site.entity';
 import { Repository } from 'typeorm';
+import { CreateSiteDto } from './dto/create-site.dto';
+import { Project } from '../projects/project.entity';
 
 @Injectable()
 export class SitesService {
     constructor(
+        @InjectRepository(Project)
+        private projectRepository: Repository<Project>,
         @InjectRepository(Site)
         private siteRepository: Repository<Site>
     ) { }
+
+    async create(createSiteDto: CreateSiteDto) {
+        const project = await this.projectRepository.findOneBy({ id: createSiteDto.projectId });
+        if (!project) {
+            throw new Error('Project with such id not found');
+        }
+        const site = new Site();
+        site.title = createSiteDto.title;
+        site.projectId = createSiteDto.projectId;
+        return await this.siteRepository.save(site);
+    }
 }
