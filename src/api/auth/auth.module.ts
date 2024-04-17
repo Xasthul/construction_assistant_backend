80ba222f0dc185dd39d45af387f40d3ad46b8d07
nazhon <nazargon2003@gmail.com>
2024-04-17 22:07:44 +0300

    Add expires in option for jwt tokens in auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -5,7 +5,13 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, JwtModule.register({ secret: '733b4901174c1d06f70e7855fd6330490ca7069cb31eef6cb78a6e15de1356d1' })],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: '733b4901174c1d06f70e7855fd6330490ca7069cb31eef6cb78a6e15de1356d1',
+      signOptions: { expiresIn: '7d' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
