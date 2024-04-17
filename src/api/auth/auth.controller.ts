e39463a27afe1594880d323cc0cb92754eb0af5c
nazhon <nazargon2003@gmail.com>
2024-04-17 22:09:27 +0300

    Add login route in auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService,
+    ) { }
 
     @Post('signup')
     signUp(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
+    }
+
+    @Post('login')
+    login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto);
     }
 }
