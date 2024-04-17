ab565a319267936db76816edb06a05896968291e
nazhon <nazargon2003@gmail.com>
2024-04-17 22:04:38 +0300

    Add login in auth service

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    async login(loginDto: LoginDto) {
+        const user = await this.usersService.findOne(loginDto.email);
+
+        const passwordsMatched = await bcrypt.compare(loginDto.password, user.password);
+        if (!passwordsMatched) {
+            throw new UnauthorizedException();
+        }
+
+        const payload = { email: user.email, sub: user.id };
+        return { accessToken: this.jwtService.sign(payload) };
+    }
+}
