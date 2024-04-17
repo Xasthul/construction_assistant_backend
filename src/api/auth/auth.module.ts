3afa99034d205cacca09c5b1b71d7ec09b41ce0c
nazhon <nazargon2003@gmail.com>
2024-04-17 22:38:27 +0300

    Register jwt strategy in auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -3,16 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { authConstants } from './auth.constants';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secret: '733b4901174c1d06f70e7855fd6330490ca7069cb31eef6cb78a6e15de1356d1',
+      secret: authConstants.secret,
       signOptions: { expiresIn: '7d' }
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }
