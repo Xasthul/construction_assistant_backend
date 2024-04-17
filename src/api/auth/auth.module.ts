1dfc30cb7b6344911d3e491777519d5ea407788a
nazhon <nazargon2003@gmail.com>
2024-04-17 21:58:26 +0300

    Register jwt module in auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule.register({ secret: '733b4901174c1d06f70e7855fd6330490ca7069cb31eef6cb78a6e15de1356d1' })],
   controllers: [AuthController],
   providers: [AuthService]
 })
