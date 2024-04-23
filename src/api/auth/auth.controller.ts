fcc5f63a5c231296a3a114204383d9a811c46599
nazhon <nazargon2003@gmail.com>
2024-04-23 22:29:21 +0300

    Add swagger tags for signUp in auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -3,8 +3,10 @@ import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
     constructor(
         private usersService: UsersService,
@@ -12,6 +14,11 @@ export class AuthController {
     ) { }
 
     @Post('signup')
+    @ApiOperation({ summary: 'Register new user' })
+    @ApiResponse({
+        status: 201,
+        description: 'Success'
+    })
     signUp(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
     }
